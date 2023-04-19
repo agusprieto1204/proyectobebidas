a139c863d2c9c3ff37e8d920499c8d39ec1d4b44
agusprieto1204 <agusprieto999@gmail.com>
2023-04-18 23:25:14 -0300

    proyecto

---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Bebidas </h3>
+            <div>
+                <button>Fernet</button>
+                <button>Cerveza</button>
+                <button>Gancia</button>
+                <button>Vino</button>
+
+            </div>
+        </nav>
+    ) 
+
+    }
+ export default NavBar
